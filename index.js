dfdb3fb95b37f1509031e7b9a774b74049573895
Devon D'Apuzzo <devon.dapuzzo@gmail.com>
2016-09-29 21:00:02 -0500

    allow CORS requests

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+//TEMPORARY CAN BE HUGE SECURITY FLAW
+// WITH THIS APP IS OPEN TO ANYONE TO CREATE 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, POST","PUT");
+  next();
+});
 
 var getFromDatabase = function(queryString, callback){
   var results = [];
