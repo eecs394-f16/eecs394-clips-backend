46f757358206a1a13946e984e3da435f2ab2a777
Devon D'Apuzzo <djdapz@aol.com>
2016-10-14 21:52:08 -0500

    user endpoint added

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ var connection = {
 }
 
 var db = pgp(connection);
-//var connectionString = "postgres://yaxjxskpbuqhrp:a59tP-DWsdUu06qjF05JhGDcNg@ec2-54-235-124-2.compute-1.amazonaws.com:5432/d5f2jhp9vvsgqh"
+
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
 //TEMPORARY CAN BE HUGE SECURITY FLAW
-// WITH THIS APP IS OPEN TO ANYONE TO CREATE 
+// WITH THIS APP IS OPEN TO ANYONE TO CREATE
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
@@ -27,7 +27,7 @@ app.all('/*', function(req, res, next) {
   next();
 });
 
-var getFromDatabase = function(queryString, callback){
+var interactWithDatabase = function(queryString, callback){
   var results = [];
   console.log(queryString);
   db.any(queryString, [true])
@@ -46,7 +46,7 @@ app.get('/coupon/all', function(req,res){
             + " FROM coupon c"
             + " INNER JOIN business b"
             + " on c.business_id = b.id";
-  getFromDatabase(queryString, function(results, error){
+  interactWithDatabase(queryString, function(results, error){
     if(error){
       res.status(500).json(error)
     }else{
@@ -64,7 +64,7 @@ app.get('/coupon/:id', function(req,res){
             + " INNER JOIN business b"
             + " ON c.business_id = b.id"
             + " WHERE  c.id = " + couponId;
-  getFromDatabase(queryString, function(results, error){
+  interactWithDatabase(queryString, function(results, error){
     if(error){
       res.status(500).json(error)
     }else{
@@ -77,7 +77,7 @@ app.get('/coupon/:id', function(req,res){
 app.get('/business/all', function(req,res){
   console.log('here')
   var queryString = "SELECT * FROM business";
-  getFromDatabase(queryString, function(results, error){
+  interactWithDatabase(queryString, function(results, error){
     if(error){
       res.status(500).json(error)
     }else{
@@ -94,7 +94,7 @@ app.get('/business/coupons/:id', function(req,res){
             + " INNER JOIN business b"
             + " ON c.business_id = b.id"
             + " WHERE  b.id = " + businessId;
-  getFromDatabase(queryString, function(results, error){
+  interactWithDatabase(queryString, function(results, error){
     if(error){
       res.status(500).json(error)
     }else{
@@ -108,7 +108,7 @@ app.get('/business/:id', function(req,res){
   var queryString = "SELECT b.*"
             + " FROM business b"
             + " WHERE  b.id = " + businessId;
-  getFromDatabase(queryString, function(results, error){
+  interactWithDatabase(queryString, function(results, error){
     if(error){
       res.status(500).json(error)
     }else{
@@ -116,6 +116,76 @@ app.get('/business/:id', function(req,res){
     }
   });
 })
+
+app.get('/user/:userid/coupons', function(req,res){
+  var userID =req.params.userid;
+  var queryString = "SELECT      c.*"
+                  +" FROM        coupon c"
+                  +" INNER JOIN  users_coupon uc"
+                  +"   ON        uc.coupon_id=c.id"
+                  +" WHERE uc.users_id = " + userID;
+
+  interactWithDatabase(queryString, function(results, error){
+    if(error){
+      res.status(500).json(error)
+    }else{
+      res.json(results);
+    }
+  });
+})
+
+app.post('/user/:userid/coupons/:couponid', function(req,res){
+  var userID =req.params.userid;
+  var couponID = req.params.couponid;
+  var queryString = "INSERT INTO users_coupon (coupon_id, users_id)"
+                  +" VALUES("+couponID+","
+                  +" (SELECT id"
+                  +" FROM users"
+                  +" WHERE id = "+userID+"));"
+
+  interactWithDatabase(queryString, function(results, error){
+    if(error){
+      res.status(500).json(error)
+    }else{
+      res.json({success:true});
+    }
+  });
+})
+
+app.delete('/user/:userid/coupons/:couponid', function(req, res){
+  var userID =req.params.userid;
+  var couponID = req.params.couponid;
+  var queryString = "DELETE FROM users_coupon"
+                  +" WHERE users_id = "+userID+" AND coupon_id = "+couponID+";"
+  interactWithDatabase(queryString, function(results, error){
+    if(error){
+      res.status(500).json({
+        success: false,
+        error: error
+      })
+    }else{
+      res.json({success:true});
+    }
+  });
+})
+
+app.get('/login/:email/:password', function(req, res){
+  var email =req.params.email;
+  var password = req.params.password;
+  var queryString = "SELECT FROM users"
+                  +" WHERE users_id = "+userID+" AND coupon_id = "+couponID+";"
+  interactWithDatabase(queryString, function(results, error){
+    if(error){
+      res.status(500).json({
+        success: false,
+        error: error
+      })
+    }else{
+      res.json({success:true,
+                user_data: results});
+    }
+  });
+});
 
 
 app.get('/test', function(req,res){
